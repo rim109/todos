2d8c03d5bba667f3de0cdcc3c053584b2a0a4493
rim109 <fla7722@naver.com>
2024-01-19 00:16:24 +0900

    feat: 에러 코드 수정

--- src/main/kotlin/com/example/todo/common/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/example/todo/common/exception/CustomExceptionHandler.kt
@@ -40,6 +40,6 @@ class CustomExceptionHandler {
     @ExceptionHandler(Exception::class)
     protected fun defaultException(ex: Exception): ResponseEntity<BaseResponse<Map<String, String>>> {
         val errors = mapOf("미처리 에러" to (ex.message ?: "Not Exception Message"))
-        return ResponseEntity(BaseResponse(ResultCode.ERROR.name, errors, ResultCode.ERROR.msg), HttpStatus.BAD_REQUEST)
+        return ResponseEntity(BaseResponse(ResultCode.ERROR.name, errors, ResultCode.ERROR.msg), HttpStatus.UNAUTHORIZED)
     }
 }
