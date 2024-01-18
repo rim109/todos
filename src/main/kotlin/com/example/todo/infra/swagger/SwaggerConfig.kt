06e31778263f4756788d5220436a945e9245ed2c
rim109 <fla7722@naver.com>
2024-01-18 18:47:25 +0900

    feat: SwaggerConfig authorize 로직 추가

--- src/main/kotlin/com/example/todo/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/example/todo/infra/swagger/SwaggerConfig.kt
@@ -3,17 +3,30 @@ package com.example.todo.infra.swagger
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 
 import org.springframework.context.annotation.Configuration
 
+private const val SECURITY_SCHEME_NAME = "authorization"
 
 @Configuration
-class SwaggerConfigs {
+class SwaggerConfig {
 
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
-        .components(Components())
+        .components(
+            Components()
+                .addSecuritySchemes(
+                    SECURITY_SCHEME_NAME, SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+        )
+        .addSecurityItem(SecurityRequirement().addList(SECURITY_SCHEME_NAME))
         .info(
             Info()
                 .title("TODO API")
